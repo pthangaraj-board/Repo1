ba4c8dcc926cf01e024d0e67d2c12584d61896d6
pthangaraj-board <65935374+pthangaraj-board@users.noreply.github.com>
2020-05-27 12:16:11 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include ""
 int main()
 {
-  print hello();
-  std::cout << std::endl;
+  print_hello();
+  std::cout << "Welcome" << std::endl;
   std:cout << "Factorial of 5: " << factorial(5) << std::endl;
   return 0;
 }
